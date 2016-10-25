d89107321dcafb02bfc32ef9a017c5f99a639271
Domagoj <domagoj.kriskovic@gmail.com>
2016-10-25 08:42:18 +0200

    version bump

--- test/recycle.spec.js
+++ test/recycle.spec.js
@@ -1,12 +1,6 @@
 import {expect} from 'chai';
-import ReCycle from '../src/recycle'
-import xstreamAdapter from 'recycle-xstream'
+import ReCycle from '../src/index'
 
 it('should throw an error', () => {
   expect(ReCycle).to.throw('No adapter provided');
-});
-
-it('should have render method', () => {  
-  let app = ReCycle(xstreamAdapter)
-  expect(app).to.have.property('render')
 });
