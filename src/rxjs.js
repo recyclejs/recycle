86196ddefadb3ec68230a1b02246b445bbd58f0d
Domagoj <domagoj.kriskovic@gmail.com>
2016-10-26 23:47:12 +0200

    recycle with react

---
+++ src/rxjs.js
@@ -0,0 +1,45 @@
+import { Observable } from 'rxjs/Observable'
+import Rx from 'rxjs/Rx'
+
+function makeSubject() {
+  var stream = new Rx.Subject();
+  var observer = {
+      next: function (x) { stream.next(x); },
+      error: function (err) { stream.error(err); },
+      complete: function () { stream.complete(); },
+  };
+  return { stream: stream, observer: observer };
+}
+
+function mergeArray(arr) {
+  return Observable.merge(...arr).share()
+}
+
+Observable.prototype.bulkMap = function(cb) {
+  return this.switchMap(val => Observable.of(...cb(val)))
+}
+Observable.prototype.reducer = function(reducer) {
+  if (arguments.length > 1) {
+    return this.bulkMap(action => {
+      let reducers = []
+      for (let i=0; i<arguments.length; i++) {
+        reducers.push({reducer: arguments[i], action})
+      }
+      return reducers
+    })
+  }
+  return this.map(action => ({reducer, action}))
+}
+Observable.prototype.filterByType = function(type) {
+  return this.filter(action => action.type === type)
+}
+
+Observable.prototype.mapCatched = function(cb) {
+  return this.catch(e => [cb(e)])
+}
+
+export {
+  Observable, 
+  makeSubject,
+  mergeArray
+}
--- src/view/hooks.js
+++
@@ -1,3 +0,0 @@
-// view hook helpers
-export const onUpdate = (fn) => ({ update: (old, {elm}) => fn(elm) })
-export const forceVal = (val) => onUpdate(elm => (typeof val === 'string') ? elm.value = val : null)
