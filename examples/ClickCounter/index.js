18500801dcbe5d2d5041a6d2492a9146be85a031
Domagoj <domagoj.kriskovic@gmail.com>
2016-11-05 17:41:30 +0100

    example using npm package

--- examples/ClickCounter/index.js
+++ examples/ClickCounter/index.js
@@ -1,7 +1,5 @@
-import createRecycle from '../../src/index'
-import ReactRxJS from '../../src/adapter/react-rxjs'
-import React from 'react'
-import ReactDOM from 'react-dom'
+import createRecycle from 'recyclejs'
+import ReactRxJS from 'recyclejs/adapter/react-rxjs'
 import WrapMultipleCounters from './WrapMultipleCounters'
 
 let recycle = createRecycle({
