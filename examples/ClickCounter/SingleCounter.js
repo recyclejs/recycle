e2ae8eb57d6186875d47714112dccfa2e1228e64
Domagoj <domagoj.kriskovic@gmail.com>
2016-11-08 11:27:19 +0100

    store connect concept

--- examples/ClickCounter/SingleCounter.js
+++ examples/ClickCounter/SingleCounter.js
@@ -46,3 +46,43 @@ export default function SingleCounterContainer() {
     },
   }
 }
+/*
+  primjer todomvc
+  todolist i todo mijenjaju todos.list
+  todolist više ne sluša childSources, samo poziva todo container u view-u
+  kako se todo.list promijeni, mijenjaju se oba containera
+  (bitno je da se promijene prije rendera)
+
+  novi ciklus
+    todolist iz svog containera dobiva listu todoa iz storea, poziva ih u viewu
+    todo container se promijenio u međuvremenu i wrapa prez komponentu u kombinaciji storeStatea i propsa od todolist
+
+  zašto container za todo?
+    tako da se todolist ne brine kad pobrisat neki todo (todo sam sebe briše)
+    todolist tako ne sluša niti jednu akciju todo-a, brine samo za sebe
+
+  komponente su decouplane, komuniciraju tako šta mijenjaju isti state (sa svojim reducerima)
+
+  promjena statea preko immutable
+    - na svaki connect, dobije se kopija storea
+    - na svaku promjenu reducera, sejva se nova kopija u store
+
+  connect priprema komponentu za korištenje, ne radi force update
+  ako se promijeni store iz vana, rendane komponente se ne mijenjaju!
+  ali parent koji ih poziva, sad poziva "nove" komponente (promjenom statea od strane parenta, on sam radi update)
+*/
+/*function SingleCounterContainer(connect) {
+  return connect('todos.list', function (storeState) {
+    return {
+      initialState: storeState,
+
+      actions: (sources, props) => {
+        return sources.childrenActions
+      },
+
+      view: (state, props, jsx) => {
+        return <SingleCounter {...props} />
+      },
+    }
+  })
+}*/
