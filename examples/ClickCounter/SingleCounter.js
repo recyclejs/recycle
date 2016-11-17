b52a0968683bd8c0ff1c7ce7677af178232b177f
Domagoj <domagoj.kriskovic@gmail.com>
2016-11-17 13:29:13 +0100

    single counter clean

--- examples/ClickCounter/SingleCounter.js
+++ examples/ClickCounter/SingleCounter.js
@@ -36,28 +36,3 @@ export default function SingleCounter () {
     }
   }
 }
-
-class Bla extends React.Component {
-
-  render (jsx) {
-    // when used inside Recycle component
-    // jsx handler will be passed in render method of a react component
-    return (
-      <SingleCounter key={this.props.id} />
-    )
-  }
-}
-
-function SingleCounterContainer (props) {
-  return {
-    storePath: 'test',
-
-    actions: (sources, props) => {
-      return sources.childrenActions
-    },
-
-    view: (jsx, props, state) => {
-      return <SingleCounter />
-    }
-  }
-}
