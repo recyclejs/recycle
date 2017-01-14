2de334f13ab68f33c4e5345e63244e47a35b0e78
Domagoj <domagoj.kriskovic@gmail.com>
2017-01-14 19:08:55 +0100

    websockets default adapter

--- examples/Websocket/src/index.js
+++ examples/Websocket/src/index.js
@@ -4,13 +4,13 @@ import WebsocketEcho from './components/WebsocketEcho'
 import WebsocketDriver from './drivers/websocket'
 
 // RECYCLE DEFINED USING DEFAULT ADAPTER
-// import Recycle from 'recyclejs'
+import Recycle from 'recyclejs'
 
 // RECYCLE DEFINED USING CUSTOM ADAPTER
-import Rx from 'rxjs/Rx'
-import streamAdapter from 'recyclejs/adapter/rxjs'
-import componentAdapter, { createRecycle } from 'recyclejs/adapter/react'
-const Recycle = createRecycle(componentAdapter(React), streamAdapter(Rx))
+// import Rx from 'rxjs/Rx'
+// import streamAdapter from 'recyclejs/adapter/rxjs'
+// import componentAdapter, { createRecycle } from 'recyclejs/adapter/react'
+// const Recycle = createRecycle(componentAdapter(React), streamAdapter(Rx))
 
 ReactDOM.render((
   <Recycle root={WebsocketEcho} drivers={[WebsocketDriver]} />
