b31b63be7120455208280db06d940c11a5b21268
Domagoj <domagoj.kriskovic@gmail.com>
2017-01-22 18:51:59 +0100

    changed api

--- src/node.js
+++ src/node.js
@@ -1,11 +1,26 @@
 import Recycle from './recycle'
 import streamAdapter from './adapter/rxjs'
 
-export default function (Rx, children) {
-  const recycle = Recycle(streamAdapter(Rx))
+export default function (params) {
+  if (!params) {
+    throw new Error('Invalid params')
+  }
+  if (!params.streamAdapter) {
+    throw new Error('Missing streamAdapter')
+  }
+
+  const recycle = Recycle(streamAdapter(params.streamAdapter))
   const rootComponent = recycle.createComponent({})
-  children.forEach(child => {
-    recycle.createComponent(child, null, rootComponent)
-  })
+
+  if (params.children) {
+    params.children.forEach(child => {
+      recycle.createComponent(child, null, rootComponent)
+    })
+  }
+
+  if (params.drivers) {
+    recycle.use(params.drivers)
+  }
+
   return recycle
 }
