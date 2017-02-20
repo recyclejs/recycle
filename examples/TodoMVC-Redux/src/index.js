e722aaa15c82aaca5335fda7d530b5ae8d19a122
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-02-20 10:53:31 +0100

    TodoMVC redux default adapter

--- examples/TodoMVC-Redux/src/index.js
+++ examples/TodoMVC-Redux/src/index.js
@@ -5,12 +5,12 @@ import 'todomvc-app-css/index.css'
 import reduxDriver from 'recyclejs/drivers/redux'
 
 // RECYCLE DEFINED USING DEFAULT ADAPTER
-// import Recycle from 'recyclejs'
+import Recycle from 'recyclejs'
 
 // RECYCLE DEFINED USING CUSTOM ADAPTER
-import Rx from 'rxjs/Rx'
-import createRecycle from 'recyclejs/react'
-const Recycle = createRecycle(React, Rx)
+// import Rx from 'rxjs/Rx'
+// import createRecycle from 'recyclejs/react'
+// const Recycle = createRecycle(React, Rx)
 
 // 'REDUX-WAY' FOR DEFINING REDUCERS
 // import rootReducer from './reducers'
