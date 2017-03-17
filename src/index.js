faa94292f5e9b4331bcdfe5b9070f95e96c2dd92
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-03-17 15:07:10 +0100

    createRecycle changed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,26 @@ import typeChecker from './drivers/typeChecker'
 export default function (Rx) {
   const recycle = createRecycle(streamAdapter(Rx))
   recycle.use(initEmitter, childrenProp, typeChecker)
-  return recycle
+
+  // "headless root component"
+  const rootComponent = recycle.createComponent(() => ({}), null, false)
+  // rewriting createComponent to set rootComponent as its parent
+  const createComponent = function (constructor, props, componentDefinition) {
+    return recycle.createComponent(constructor, props, rootComponent, componentDefinition)
+  }
+
+  const createReactComponent = function (reactComponent, props) {
+    if (!recycle.get('createReactComponent')) {
+      throw new Error('createReactComponent function is not set')
+    }
+    return recycle.get('createReactComponent')(reactComponent, props, rootComponent)
+  }
+
+  return {
+    ...recycle,
+    createReactComponent,
+    createComponent
+  }
 }
 
 function type (type) {
