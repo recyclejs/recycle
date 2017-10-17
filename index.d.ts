6a79bccf0590159581999552b2c3ca525d876d5e
eduardonuzzi <eduardomnuzzi@gmail.com>
2017-10-17 10:36:20 -0200

    started working on JSDoc

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,9 @@
  * TODO: Insert JSDoc
  */
 
-
 import {Observable} from 'rxjs'
-import {JSX} from 'react'
 import * as React from 'react'
+import {Action} from 'redux'
 
 
 export = recycle
@@ -13,8 +12,8 @@ declare const recycle: recycle.Recycle
 export as namespace recycle
 
 
-type ReducerFn<S> = (state: S) => S
-type ReducerObservableFn<S> = (stream: Observable<Event>) => Observable<{ reducer: ReducerFn<S>, event: Event }>
+type ReducerFn<S, R> = (state: S, value?: any, store?: R) => S
+type ReducerObservableFn<S, R> = (stream: Observable<Event>) => Observable<{ reducer: ReducerFn<S, R>, event: Event }>
 
 
 type DomEvent =
@@ -43,14 +42,47 @@ type DomEvent =
 
 declare module 'rxjs/Observable' {
     interface Observable<T> {
-        reducer(reducerFn: ReducerFn): ReducerObservableFn
+        /**
+         *  Acts like a Redux reducer, it receives you local component state (and your Redux state, if you are using Redux),
+         *  and returns a new local state
+         *
+         * @template S Your Component local state
+         * @template R Your Redux state
+         * @param {(state: S, store: R) => S} reducerFn PS: Param store only appears if you are using Redux (Component is inside Provider)
+         * @example
+         *
+         * sources.select('button')
+         *    .addListener('onClick')
+         *    .reducer((state, eventValue) => {
+         *        ...state,
+         *        counter: state.counter + eventValue
+         *    })
+         *
+         */
+        reducer<S, R>(reducerFn: ReducerFn<S, R>): ReducerObservableFn<S, R>
     }
 }
 
 
 declare namespace recycle {
 
-    interface Listeners<S> {
+    interface Listeners {
+
+        /**
+         * Register a new DOM listener and returns a stream of DOM events
+         *
+         * @param event DOM Event to listen to
+         * @returns {Observable<Event>}
+         * @example
+         *
+         * sources.select('button')
+         *     .addListener('onClick')
+         *     .reducer(state => {
+         *         ...state,
+         *         counter: state.counter + 1
+         *     }),
+         *
+         */
         addListener: (event: DomEvent) => Observable<Event>
     }
 
@@ -63,29 +95,106 @@ declare namespace recycle {
         'componentDidUpdate' |
         'componentWillUnmount'
 
-    interface Sources<S> {
-        select: (tagName: string) => recycle.Listeners
-        select: (childComponent: string) => recycle.Listeners
+    interface Sources<S, R> {
+
+        /**
+         * This method selects a element by tagName or a ChildComponent and returns a recycle.Listeners
+         *
+         * @param param
+         * @returns {recycle.Listeners} Returns { addListener: (event: DomEvent) => Observable<Event> }
+         * @example
+         *
+         * PS: Assuming i have a <button></button>
+         * sources.select('button')
+         *
+         * or
+         *
+         * PS: Assuming i have a <ChildComponent></ChildComponent> as a child element.
+         * sources.select(ChildComponent)
+         *
+         */
+        select: (param: string) => recycle.Listeners
+
+        /**
+         * This method selects a element by a CSS class and returns a recycle.Listeners
+         *
+         * @param className
+         * @returns {recycle.Listeners} Returns { addListener: (event: DomEvent) => Observable<Event> }
+         * @example
+         *
+         * * PS: Assuming i have a <Element class='cssClass'></Element> as a child element.
+         * sources.select('cssClass')
+         *
+         */
         selectClass: (className: string) => recycle.Listeners
+
+        /**
+         * This method selects a element by id and returns a recycle.Listeners
+         *
+         * @param id
+         * @returns {recycle.Listeners} Returns { addListener: (event: DomEvent) => Observable<Event> }
+         * @example
+         *
+         * PS: Assuming i have a <Element id='elementId'></Element> as a child element.
+         * sources.select('elementId')
+         *
+         */
         selectId: (id: string) => recycle.Listeners
 
-        store: Observable<S>
+        /**
+         * If you are using Redux (component is inside Provider) this will return a stream of your redux state
+         *
+         * @template R is a interface representing your Redux state
+         * @returns {Observable<R>} Returns a Observable with your latest Redux state
+         * @example
+         *
+         * PS: Assuming R is { count: number }
+         * sources.store.map((state: { count: number }) => state.count)
+         *
+         */
+        store?: Observable<R>
+
+        /**
+         * Returns a stream of your local component state
+         *
+         * @template S is a interface representing your Component state
+         * @returns {Observable<S>} Returns a Observable with your latest component state
+         * @example
+         *
+         * sources.select('input')
+         *     .addListener('onKeyPress')
+         *     .filter(e => e.key === 'Enter')
+         *     .withLatestFrom(sources.state)
+         *     .map(([e, state]) => state.someStateValue)
+         *
+         */
         state: Observable<S>
 
+        /**
+         * Returns a stream of component lifecycle events
+         *
+         * @returns {Observable<ReactLifeCycle>}
+         * @example
+         *
+         * sources.lifecycle
+         *     .filter(e => e === 'componentDidMount')
+         *     .do(something)
+         *
+         */
         lifecycle: Observable<ReactLifeCycle>
     }
 
-    interface Params<S> {
+    interface Params<S, R> {
         propTypes?: any
         displayName?: string
         initialState?: S
-        dispatch?: (sources: recycle.Sources<S>) => Observable
-        update?: (sources: recycle.Sources<S>) => ReducerObservableFn<S>[]
-        effects?: (sources: recycle.Sources<S>) => Observable[]
+        dispatch?: (sources: recycle.Sources<S, R>) => Observable<Action>
+        update?: (sources: recycle.Sources<S, R>) => ReducerObservableFn<S, R>[]
+        effects?: (sources: recycle.Sources<S, R>) => Observable<any>[]
         view?: (props: any, state: S) => JSX.Element
     }
 
     interface Recycle {
-        <S>(params: recycle.Params<S>): React.Component
+        <S, R=any>(params: recycle.Params<S, R>): React.Component
     }
 }
