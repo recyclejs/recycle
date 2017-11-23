522c53a1bca2e22baf598c0140caafa232568df8
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-11-23 01:45:52 +0100

    check for Rx

--- src/customRxOperators.js
+++ src/customRxOperators.js
@@ -2,7 +2,7 @@ export const reducer = (reducerFn) => (stream) =>
   stream.map(event => ({ reducer: reducerFn, event }))
 
 export default Rx => {
-  if (!Rx.Observable.prototype.reducer) {
+  if (Rx && Rx.Observable && !Rx.Observable.prototype.reducer) {
     Rx.Observable.prototype.reducer = function (reducerFn) {
       return reducer(reducerFn)(this)
     }
